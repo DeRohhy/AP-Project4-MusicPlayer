15ace0536ea3403c5ddf2678dc7c450e46e713aa
RohhyDohhy <rohhy.business@gmail.com>
2026-06-19 00:47:19 +0330

    fix(player): fix play() and pause() features

--- include/player/Player.h
+++ include/player/Player.h
@@ -9,6 +9,10 @@ public:
     Player();
     ~Player();
 
+    bool isPlaying();
+    float getTime();
+    std::string getSoundPath() { return sound_path; }
+
     void setSound(const std::string& path);
 
     void play();

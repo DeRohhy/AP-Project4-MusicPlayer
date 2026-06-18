fd32c17dc93928876a35d7bc751c68a30806e866
RohhyDohhy <rohhy.business@gmail.com>
2026-06-19 00:49:08 +0330

    refractor(ui): remove ConfigManager and MusicLibrary member variables from UIComponent class

--- include/ui/UIComponent.h
+++ include/ui/UIComponent.h
@@ -2,13 +2,13 @@
 
 #include <ncurses.h>
 #include <string>
-#include <io/ConfigManager.h>
+
 
 class UIComponent
 {
 public:
-    UIComponent(int y, int x, int h, int w, ConfigManager& _config_manager)
-        : start_y(y), start_x(x), height(h), width(w), config_manager(_config_manager)
+    UIComponent(int y, int x, int h, int w) 
+        : start_y(y), start_x(x), height(h), width(w) 
         {
             window = newwin(height, width, start_y, start_x);
             refresh();
@@ -16,7 +16,10 @@ public:
     
     virtual ~UIComponent() { delwin(window); }
 
+    void setFocus(bool val) { is_focused = val; };
+
     virtual void draw() = 0;
+    virtual void handleInput(int op) = 0;
 
     // Truncate to `width` chars, ending with "..." if it was too long
     std::string fit(const std::string& s, size_t max_width);
@@ -31,5 +34,6 @@ protected:
     int start_x;
     int height;
     int width;
-    ConfigManager config_manager;
+
+    bool is_focused;
 };

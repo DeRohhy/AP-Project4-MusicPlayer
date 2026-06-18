9f665ad0fc785f7627090e96f02169223bdd4db2
RohhyDohhy <rohhy.business@gmail.com>
2026-06-19 00:47:58 +0330

    fix(player): fix play() and pause() features

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -17,16 +17,34 @@ Player::~Player()
 
 void Player::setSound(const std::string& path)
 {
-    if (ma_sound_init_from_file(&engine, path.c_str(), 0, NULL, NULL, &sound) != MA_SUCCESS)
+    if (ma_sound_init_from_file(&engine, path.c_str(), MA_SOUND_FLAG_STREAM, NULL, NULL, &sound) != MA_SUCCESS)
         throw std::runtime_error("Failed to set sound");
+    sound_path = path;
+}
+
+bool Player::isPlaying()
+{
+    return ma_sound_is_playing(&sound);
+}
+
+float Player::getTime()
+{
+    ma_uint64 frames = 0;
+    ma_sound_get_cursor_in_pcm_frames(const_cast<ma_sound*>(&sound), &frames);
+
+    return (float)frames / (float)ma_engine_get_sample_rate(
+        const_cast<ma_engine*>(&engine)
+    );
 }
 
 void Player::play()
 {
-    ma_sound_start(&sound);
+    if (!isPlaying())
+        ma_sound_start(&sound);
 }
 
 void Player::pause()
 {
-    ma_sound_stop(&sound);
+    if (isPlaying())
+        ma_sound_stop(&sound);
 }

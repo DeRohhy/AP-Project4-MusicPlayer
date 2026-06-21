678d46818a42b7b9c569656b290e808cbf2b19e8
RohhyDohhy <rohhy.business@gmail.com>
2026-06-21 17:31:00 +0330

    feat(Playlist): add Playlist::getSongAmount()

--- include/model/Playlist.h
+++ include/model/Playlist.h
@@ -9,12 +9,12 @@ class Playlist
 public:
     Playlist() = default;
 
-    std::string getPlaylistName() { return playlist_name; }
-
     std::string getPlaylistName() const { return playlist_name; }
 
     void setPlaylistName(std::string name) { playlist_name = name; }
     void addSong(const Song* song) { songs.push_back(song); }
+    size_t getSongAmount() const { return songs.size(); }
+    const Song* getSong(int i) const { return songs[i]; }
 
     void sortByTitle();
     void sortByArtist();

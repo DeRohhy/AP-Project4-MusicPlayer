c28360070afb61984345fa34572682edb3500c9b
RohhyDohhy <rohhy.business@gmail.com>
2026-06-22 21:14:40 +0330

    feat(Player): add nextSong(), previousSong() and advanceSong() with shuffle and playback_mode

--- include/player/Player.h
+++ include/player/Player.h
@@ -3,6 +3,14 @@
 #include "../external/miniaudio.h"
 #include "model/Playlist.h"
 #include <string>
+#include <vector>
+
+enum class PlaybackMode
+{
+    NO_REPEAT,
+    REPEAT_ONE,
+    REPEAT_ALL,
+};
 
 class Player
 {
@@ -14,17 +22,34 @@ public:
     float getTime();
     std::string getSoundPath() { return sound_path; }
     bool isFinished() { return ma_sound_at_end(&sound); }
+    bool isAtEndOfQueue() const; // ignores playback mode
+    bool isAtStartOfQueue() const; // ignores playback mode
 
     void setSound(const std::string& path);
-    void setPlaylist(Playlist* playlist);
+    void setPlaylist(const Playlist* playlist);
+    void setShuffle(bool val);
+    void setPlaybackMode(PlaybackMode mode) { playback_mode = mode; }
 
     void play();
     void pause();
     void seekBy(int seconds);
+    void buildQueue();
+    void buildQueueFrom(int index);
+    void nextSong();
+    void previousSong();
+    void advanceTrack();
 
 private:
     ma_engine engine;
     ma_sound sound;
     std::string sound_path;
     bool sound_initialized; 
+
+    const Playlist* active_playlist; 
+    bool shuffle;
+    PlaybackMode playback_mode;
+    std::vector<int> play_queue;
+    int queue_index;
+
+    int getCurrentSongIndex();
 };
--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -1,9 +1,11 @@
 #define MINIAUDIO_IMPLEMENTATION
 
 #include "player/Player.h"
-#include <iostream>
+#include <algorithm>
+#include <random>
 
-Player::Player() : sound_initialized { false }
+Player::Player() : sound_initialized { false }, active_playlist { nullptr },
+                   shuffle { false }, playback_mode { PlaybackMode::NO_REPEAT }
 {
     if (ma_engine_init(NULL, &engine) != MA_SUCCESS)
         throw std::runtime_error("Failed to initialize audio engine");
@@ -30,6 +32,43 @@ void Player::setSound(const std::string& path)
 
     sound_initialized = true;
     sound_path = path;
+
+    queue_index = getCurrentSongIndex();
+}
+
+void Player::setPlaylist(const Playlist* playlist)
+{
+    active_playlist = playlist;
+
+    if (!sound_initialized || !active_playlist)
+        return;
+
+    if (shuffle)
+    {
+        buildQueueFrom(getCurrentSongIndex());
+        queue_index = 0;
+    }
+    else
+    {
+        buildQueue();
+        queue_index = getCurrentSongIndex();        
+    }
+}
+
+void Player::setShuffle(bool val)
+{
+    shuffle = val;
+
+    if (val)
+    {
+        buildQueueFrom(getCurrentSongIndex());
+        queue_index = 0;
+    }
+    else
+    {
+        buildQueue();
+        queue_index = getCurrentSongIndex();
+    }
 }
 
 bool Player::isPlaying()
@@ -49,6 +88,21 @@ float Player::getTime()
     );
 }
 
+bool Player::isAtEndOfQueue() const
+{
+    if (!sound_initialized || !active_playlist)
+        return false;
+    return queue_index + 1 == active_playlist->getSongAmount();
+}
+
+bool Player::isAtStartOfQueue() const
+{
+    if (!sound_initialized || !active_playlist)
+        return false;
+    return queue_index == 0;
+}
+
+
 void Player::play()
 {
     if (sound_initialized && !isPlaying())
@@ -64,7 +118,7 @@ void Player::pause()
 void Player::seekBy(int seconds)
 {
     if (!sound_initialized) return;
-
+    
     ma_uint64 cursor, length;
     ma_sound_get_cursor_in_pcm_frames(&sound, &cursor);
     ma_sound_get_length_in_pcm_frames(&sound, &length);
@@ -78,4 +132,127 @@ void Player::seekBy(int seconds)
         new_frame = (ma_int64)(length - 1);
 
     ma_sound_seek_to_pcm_frame(&sound, (ma_uint64)new_frame);
+}
+
+void Player::buildQueue()
+{
+    if (!active_playlist)
+    {
+        play_queue.clear();
+        return;
+    }
+
+    size_t song_count = active_playlist->getSongAmount(); 
+
+    if (song_count == 0)
+    {
+        play_queue.clear();
+        return;
+    }
+
+    play_queue.resize(song_count);
+    for (size_t i = 0; i < song_count; i++)
+        play_queue[i] = i;
+
+    if (!shuffle)
+        return;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(play_queue.begin(), play_queue.end(), gen);
+}
+
+void Player::buildQueueFrom(int index) // place the given song index at the start of our play queue
+{
+    if (!active_playlist)
+    {
+        play_queue.clear();
+        return;
+    }
+
+    size_t song_count = active_playlist->getSongAmount(); 
+
+    if (song_count == 0)
+    {
+        play_queue.clear();
+        return;
+    }
+
+    play_queue.resize(song_count);
+    for (size_t i = 0; i < song_count; i++)
+        play_queue[i] = i;
+
+    if (song_count == 1)
+        return;
+
+    if (index >= 0 && index < song_count) 
+        std::swap(play_queue[index], play_queue[0]);
+
+    if (!shuffle)
+        return;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(play_queue.begin() + 1, play_queue.end(), gen);
+}
+
+int Player::getCurrentSongIndex()
+{
+    if (!sound_initialized || !active_playlist)
+        return -1;
+
+    for (size_t i = 0; i < active_playlist->getSongAmount(); i++)
+    {
+        if (sound_path == active_playlist->getSong(play_queue[i])->getPath())
+            return i;
+    }
+
+    return -1;
+}
+
+void Player::nextSong()
+{
+    if (!active_playlist)
+        return;
+    
+    size_t song_count = active_playlist->getSongAmount();
+    if (!song_count)
+        return;
+    
+    queue_index = (queue_index + 1 >= song_count) ? 0 : queue_index + 1;
+
+    setSound(active_playlist->getSong(play_queue[queue_index])->getPath());
+    play();
+}
+
+void Player::previousSong()
+{
+    if (!active_playlist)
+        return;
+    
+    size_t song_count = active_playlist->getSongAmount();
+    if (!song_count)
+        return;
+    
+    queue_index = (queue_index > 0) ? queue_index - 1 : song_count - 1;
+
+    setSound(active_playlist->getSong(play_queue[queue_index])->getPath());
+    play();
+}
+
+void Player::advanceTrack()
+{
+    switch (playback_mode)
+    {
+    case PlaybackMode::NO_REPEAT:
+        if (!isAtEndOfQueue())
+            nextSong();
+        break;
+    case PlaybackMode::REPEAT_ALL:
+        nextSong();
+        break;
+    case PlaybackMode::REPEAT_ONE:
+        play();
+        break;
+    }
 }
